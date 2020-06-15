6516642acae2b6eccb9495f9962251e868c2594e
Melissa Bykersma <melissabykersma@gmail.com>
2020-06-15 16:14:31 +1000

    added background to search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,20 @@
 import React from "react"
 import { View, Text, StyleSheet } from "react-native"
+import SearchBar from "../components/SearchBar"
 
 const SearchScreen = () => {
 	return (
-		<View>
+		<View style={styles.background}>
 			<Text>Search Screen</Text>
+			<SearchBar />
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	background: {
+		backgroundColor: "white",
+	},
+})
 
 export default SearchScreen
